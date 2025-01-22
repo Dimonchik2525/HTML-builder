1e7c70f263c2583c26ef7ac873123b5277753040
Dzmitry Saldatsenka <dima.soldatenko26@gmail.com>
2025-01-22 15:52:11 +0400

    fix:uncommit code

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,4 +1,4 @@
-/*const path = require('path');
+const path = require('path');
 const fs = require('fs').promises;
 
 
@@ -13,37 +13,9 @@ async function addClasses() {
       if (file.isFile() && extension == 'css') {
          let content = await fs.readFile(path.resolve(stylesPath, file.name), 'utf-8')
          result.push(content)
-         // console.log(content);
-         //  console.log(content.toString());
       }
    }
    await fs.writeFile(bundlePath, result.join('\n'))
 }
-addClasses()*/
-const fs = require('fs');
-const path = require('path');
+addClasses()
 
-async function buildCSSBundle() {
-   const stylesDir = path.join(__dirname, 'styles');
-   const outputDir = path.join(__dirname, 'project-dist');
-   const bundleFile = path.join(outputDir, 'bundle.css');
-
-   try {
-      await fs.promises.mkdir(outputDir, { recursive: true });
-      const bundleStream = fs.createWriteStream(bundleFile);
-      const items = await fs.promises.readdir(stylesDir, { withFileTypes: true });
-
-      for (const item of items) {
-         const itemPath = path.join(stylesDir, item.name);
-         if (item.isFile() && path.extname(item.name) === '.css') {
-            const cssContent = await fs.promises.readFile(itemPath, 'utf-8');
-            bundleStream.write(cssContent + '\n'); // Add newline between files
-         }
-      }
-      bundleStream.end();
-   } catch (err) {
-      console.error('Error building CSS bundle:', err);
-   }
-}
-
-buildCSSBundle();
